ea872ba577a5c4d710ca49221d9331ed74aa5fbb
Tinna <slightlydelightful@gmail.com>
2016-04-04 22:03:14 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<!--Fixed Menu-->
+
+var menu = $('nav#menu');
+
+var watcher = scrollMonitor.create( menu );
+
+watcher.lock();
+
+watcher.stateChange(function() {
+  $(menu).toggleClass('scrolled', this.isAboveViewport)
+});
